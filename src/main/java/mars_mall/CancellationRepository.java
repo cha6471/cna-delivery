e442274ea4a97ee2be8a2676b089e6d72a11dbdf
cha6471 <cha6471@gmail.com>
2020-10-13 11:39:31 +0900

    1st commit

---
+++ src/main/java/mars_mall/CancellationRepository.java
@@ -0,0 +1,8 @@
+package mars_mall;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CancellationRepository extends PagingAndSortingRepository<Cancellation, Long>{
+
+
+}
